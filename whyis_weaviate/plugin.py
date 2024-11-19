55b5e6ce07b987483ecdeb312c939492468666b0
Jamie McCusker <mccusj2@rpi.edu>
2024-11-18 20:50:03 -0500

    Ported over to the REST API.

--- whyis_weaviate/plugin.py
+++ whyis_weaviate/plugin.py
@@ -55,13 +55,13 @@ class VectorSpace:
         if self._collection is None:
             self._collection = requests.get(f'{self.db.endpoint}schema/{self.collection_id}')
             if self._collection.status_code == 404:
-                requests.post(f'{self.db.endpoint}schema/', data=dict(
-                    class=self.collection_id,
-                    vectorIndexType='hnsw',
-                    vectorIndexConfig=dict(
+                requests.post(f'{self.db.endpoint}schema/', json={
+                    'class' : self.collection_id,
+                    'vectorIndexType' : 'hnsw',
+                    'vectorIndexConfig' : dict(
                         distanceMetric=self.distance_metric
                     ),
-                    properties = [
+                    'properties' : [
                         dict(
                             name="subject", dataType='text', tokenization='field'
                         ),
@@ -69,77 +69,139 @@ class VectorSpace:
                             name="graph", dataType='text', tokenization='field'
                         ),
                     ]
-                ))
+                })
                 self._collection = requests.get(f'{self.db.endpoint}schema/{self.collection_id}')
                 print(self._collection)
         return self._collection
 
     def prepare_result(self, o, include_vector=True):
-        entity = dict(o.properties)
-        entity['uuid'] = o.uuid
+        entity = dict(o)
+        additional = entity['_additional']
+        del entity['_additional']
+        entity.update(additional)
         if include_vector:
-            entity['tensor'] = self.db.to_tensor(o.vector["default"], self.extent).tolist()
+            entity['tensor'] = self.db.to_tensor(additional['vector'], self.extent).tolist()
+            del entity['vector']
             
         return entity
 
     def _create_filter(self, subject=None, graph=None):
-        filters = [Filter.by_property(k).equal(v) for k,v
-                   in [('subject',subject), ('graph',graph)]
+        filters = [f'''{{
+  path: ["{k}"],
+  operator: Equal,
+  valueText: "{v}"
+}}
+''' for k,v in [('subject',subject), ('graph',graph)]
                    if v is not None]
-        filters = functools.reduce(lambda a, b: a & b, filters)
-        return filters
+        return ', '.join(filters)
     
     def similar(self, subject, graph=None, limit=10, offset=0):
         matches = self.get(subject, graph, include_vector=False)
         results = []
         for match in matches:
-            response = self.collection.query.near_object(
-                near_object = match['uuid'],
-                limit=limit,
-                offset=offset,
-                include_vector=True,
-                return_metadata=MetadataQuery(distance=True),
-            )
-            results = []
-            for o in response.objects:
-                entity = self.prepare_result(o)
-                entity['distance'] = o.metadata.distance
+            query = f'''{{
+  Get {{
+    {self.collection_id}(
+      nearObject: {{
+        id: "{match['id']}"
+      }},
+      limit: {limit},
+      offset: {offset}
+    ) {{
+      subject
+      graph
+      _additional {{
+        id
+        vector
+        distance
+      }}
+    }}
+  }}
+}}'''
+            response = requests.post(f'{self.db.endpoint}graphql', json={'query' : query})
+            if response.status_code != 200:
+                raise VectorDBException(response.text)
+            data = response.json()
+            if 'errors' in data:
+                raise VectorDBException(response.text)
+            responses = data['data']['Get'][self.collection_id]
+            for r in responses:
+                entity = self.prepare_result(r)
                 entity['search_graph'] = graph
-                entity['search_uuid'] = match['uuid']
+                entity['search_uuid'] = match['id']
                 entity['search_subject'] = subject
+                entity['space'] = str(self.identifier)
                 results.append(entity)
-            if len(matches) > 1:
-                results = sorted(results, key=lambda x: x['distance'])
+        if len(matches) > 1:
+            results = sorted(results, key=lambda x: x['distance'])
         return results
 
     def get(self, subject=None, graph=None, include_vector=True):
         f = self._create_filter(subject, graph)
-        response = self.collection.query.fetch_objects(filters=f,
-                                                       include_vector=include_vector)
-        results = [self.prepare_result(r, include_vector) for r in response.objects]
+        query = f'''{{
+  Get {{
+    {self.collection_id}(where: {{
+      operator: And,
+      operands: [{f}]
+    }}) {{
+      subject
+      graph
+      _additional {{
+        id
+        vector
+      }}
+    }}
+  }}
+}}'''
+        response = requests.post(f'{self.db.endpoint}graphql', json={'query' : query})
+        if response.status_code != 200:
+            raise VectorDBException(response.text)
+        data = response.json()
+        if 'errors' in data:
+            raise VectorDBException(response.text)
+
+        responses = data['data']['Get'][self.collection_id]
+        results = [self.prepare_result(r, include_vector) for r in responses]
 
         return results
 
     def search(self, tensor, limit, offset=0):
         vector, extent = self.db.to_vector(tensor)
-        response = self.collection.query.near_vector(
-            near_vector=vector,
-            limit=limit,
-            offset=offset,
-            include_vector=True,
-            return_metadata=MetadataQuery(distance=True)
-        )
-        print("searching %s"%self.identifier)
+        json_vector = json.dumps(vector.tolist())
+        query = f'''{{
+  Get {{
+    {self.collection_id}(
+      nearVector: {{
+       vector: {json_vector}
+      }},
+      limit: {limit},
+      offset: {offset}
+    ) {{
+      subject
+      graph
+      _additional {{
+        id
+        vector
+        distance
+      }}
+    }}
+  }}
+}}'''
+        response = requests.post(f'{self.db.endpoint}graphql', json={'query' : query})
+        if response.status_code != 200:
+            raise VectorDBException(response.text)
+        data = response.json()
+        if 'errors' in data:
+            raise VectorDBException(response.text)
+        responses = data['data']['Get'][self.collection_id]
         i = 0
         results = []
-        for o in response.objects:
-            entity = self.prepare_result(o)
-            entity['distance'] = o.metadata.distance
+        for r in responses:
+            entity = self.prepare_result(r)
+            entity['space'] = str(self.identifier)
             entity['rank'] = i + offset
             i += 1
             results.append(entity)
-
-        print("finished searching %s"%self.identifier)
         return results
 
 @driver('weaviate')
@@ -181,9 +243,9 @@ class WeaviateDatabase(NanopublicationListener):
         self.endpoint = f'http://{self.host}:{self.port}/v1/'
         print(self.endpoint)
         
-        r = requests.get(endpoint)
+        r = requests.get(self.endpoint)
         if r.status_code != 200:
-            raise VectorDBExcption(r)
+            raise VectorDBExcption(r.text)
 
     @property
     def spaces(self):
@@ -198,32 +260,53 @@ class WeaviateDatabase(NanopublicationListener):
     def on_publish(self, nanopub):
         g = rdflib.ConjunctiveGraph(store=nanopub.store)
         assertion_uri = str(nanopub.assertion.identifier)
+        payload = []
         for space in self.spaces.values():
             inserts = []
             removes = []
-            with space.collection.batch.dynamic() as batch:
-                for s, p, o in nanopub.assertion.triples((None,space.identifier,None)):
-                    data = self.parse_vector(o)
-                    tensor = data['tensor']
-                    vector, extent = self.to_vector(tensor)
-                    if space.extent is not None and extent != space.extent:
-                        raise VectorDBException(
-                            "Tensor shape (%s) does not match schema tensor shape (%s)." % (extent, space.extent)
-                        )
-                    batch.add_object(
-                        properties = dict(
-                            subject = str(s),
-                            graph = str(nanopub.assertion.identifier)
-                        ),
-                        vector = vector
+            for s, p, o in nanopub.assertion.triples((None,space.identifier,None)):
+                data = self.parse_vector(o)
+                tensor = data['tensor']
+                vector, extent = self.to_vector(tensor)
+                if space.extent is not None and extent != space.extent:
+                    raise VectorDBException(
+                        "Tensor shape (%s) does not match schema tensor shape (%s)." % (extent, space.extent)
                     )
-            if len(space.collection.batch.failed_objects) > 0:
-                print("Failed objects!!")
-                for o in space.collection.batch.failed_objects:
-                    print(o.message)
+                payload.append({
+                    'class' : space.collection_id,
+                    'properties' : dict(
+                        subject = str(s),
+                        graph = str(nanopub.assertion.identifier)
+                    ),
+                    'vector' : vector.tolist()
+                })
+        if len(payload) > 0:
+            response = requests.post(f'{self.endpoint}batch/objects',
+                                     json={
+                                         "fields": ["ALL"],
+                                         "objects":payload
+                                     })
+            if response.status_code != 200:
+                raise VectorDBException(response.text)
 
     def on_retire(self, nanopub):
-        collection.data.delete_many(where=Filter.by_property("graph").equal(str(nanopub.assertion.identifier)))
+        for space in self.spaces:
+            if (None, space.identifier, None) in nanopub.assertion:
+                response = requests.delete(
+                    f'{self.endpoint}batch/objects',
+                    json={
+                        'match' : {
+                            'class' : space.collection_id,
+                            'where' : {
+                                'operator' : 'Equal',
+                                'path' : ['graph'],
+                                'valueText' : str(nanopub.assertion.identifier)
+                            }
+                        }
+                    }
+                )
+                if response.status_code != 200:
+                    raise VectorDBException(response.text)
 
     def search(self, space, vector, limit, offset=0):
         try:
@@ -246,7 +329,10 @@ class WeaviateDatabase(NanopublicationListener):
         return results
 
     def similar(self, space=None, subject=None, graph=None, limit=10, offset=0):
-        spaces = [space] if space is not None or len(space) == 0 else self.spaces.keys()
+        if not space or len(space) == 0:
+            spaces =  self.spaces.keys()
+        else:
+            spaces = [space]
         try:
             spaces = [self.spaces[rdflib.URIRef(s)] for s in spaces]
             #print(spaces)
